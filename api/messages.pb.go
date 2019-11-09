73e048fef0175136c34c0869698210ec9b76a2f3
James Jeffrey <james.jeffrey@acronis.com>
2019-11-09 11:57:22 -0700

    Add auth service and regen api

--- api/messages.pb.go
+++ api/messages.pb.go
@@ -24,6 +24,94 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
+type LoginRequest struct {
+	Username             string   `protobuf:"bytes,1,opt,name=Username,proto3" json:"Username,omitempty"`
+	Password             string   `protobuf:"bytes,2,opt,name=Password,proto3" json:"Password,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *LoginRequest) Reset()         { *m = LoginRequest{} }
+func (m *LoginRequest) String() string { return proto.CompactTextString(m) }
+func (*LoginRequest) ProtoMessage()    {}
+func (*LoginRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_4dc296cbfe5ffcd5, []int{0}
+}
+
+func (m *LoginRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_LoginRequest.Unmarshal(m, b)
+}
+func (m *LoginRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_LoginRequest.Marshal(b, m, deterministic)
+}
+func (m *LoginRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LoginRequest.Merge(m, src)
+}
+func (m *LoginRequest) XXX_Size() int {
+	return xxx_messageInfo_LoginRequest.Size(m)
+}
+func (m *LoginRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_LoginRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LoginRequest proto.InternalMessageInfo
+
+func (m *LoginRequest) GetUsername() string {
+	if m != nil {
+		return m.Username
+	}
+	return ""
+}
+
+func (m *LoginRequest) GetPassword() string {
+	if m != nil {
+		return m.Password
+	}
+	return ""
+}
+
+// Token on login response needs to be set in metadata on the context for grpc calls in order to use chat service
+// the token should be sent using key token
+type LoginResponse struct {
+	Token                string   `protobuf:"bytes,1,opt,name=Token,proto3" json:"Token,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *LoginResponse) Reset()         { *m = LoginResponse{} }
+func (m *LoginResponse) String() string { return proto.CompactTextString(m) }
+func (*LoginResponse) ProtoMessage()    {}
+func (*LoginResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_4dc296cbfe5ffcd5, []int{1}
+}
+
+func (m *LoginResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_LoginResponse.Unmarshal(m, b)
+}
+func (m *LoginResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_LoginResponse.Marshal(b, m, deterministic)
+}
+func (m *LoginResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LoginResponse.Merge(m, src)
+}
+func (m *LoginResponse) XXX_Size() int {
+	return xxx_messageInfo_LoginResponse.Size(m)
+}
+func (m *LoginResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_LoginResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LoginResponse proto.InternalMessageInfo
+
+func (m *LoginResponse) GetToken() string {
+	if m != nil {
+		return m.Token
+	}
+	return ""
+}
+
 type Message struct {
 	Data                 string   `protobuf:"bytes,1,opt,name=Data,proto3" json:"Data,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -35,7 +123,7 @@ func (m *Message) Reset()         { *m = Message{} }
 func (m *Message) String() string { return proto.CompactTextString(m) }
 func (*Message) ProtoMessage()    {}
 func (*Message) Descriptor() ([]byte, []int) {
-	return fileDescriptor_4dc296cbfe5ffcd5, []int{0}
+	return fileDescriptor_4dc296cbfe5ffcd5, []int{2}
 }
 
 func (m *Message) XXX_Unmarshal(b []byte) error {
@@ -74,7 +162,7 @@ func (m *MessageResp) Reset()         { *m = MessageResp{} }
 func (m *MessageResp) String() string { return proto.CompactTextString(m) }
 func (*MessageResp) ProtoMessage()    {}
 func (*MessageResp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_4dc296cbfe5ffcd5, []int{1}
+	return fileDescriptor_4dc296cbfe5ffcd5, []int{3}
 }
 
 func (m *MessageResp) XXX_Unmarshal(b []byte) error {
@@ -102,23 +190,140 @@ func (m *MessageResp) GetData() string {
 	return ""
 }
 
+type RegisterRequest struct {
+	Username             string   `protobuf:"bytes,1,opt,name=Username,proto3" json:"Username,omitempty"`
+	Password             string   `protobuf:"bytes,2,opt,name=Password,proto3" json:"Password,omitempty"`
+	FirstName            string   `protobuf:"bytes,3,opt,name=FirstName,proto3" json:"FirstName,omitempty"`
+	LastName             string   `protobuf:"bytes,4,opt,name=LastName,proto3" json:"LastName,omitempty"`
+	Email                string   `protobuf:"bytes,5,opt,name=Email,proto3" json:"Email,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *RegisterRequest) Reset()         { *m = RegisterRequest{} }
+func (m *RegisterRequest) String() string { return proto.CompactTextString(m) }
+func (*RegisterRequest) ProtoMessage()    {}
+func (*RegisterRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_4dc296cbfe5ffcd5, []int{4}
+}
+
+func (m *RegisterRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_RegisterRequest.Unmarshal(m, b)
+}
+func (m *RegisterRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_RegisterRequest.Marshal(b, m, deterministic)
+}
+func (m *RegisterRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_RegisterRequest.Merge(m, src)
+}
+func (m *RegisterRequest) XXX_Size() int {
+	return xxx_messageInfo_RegisterRequest.Size(m)
+}
+func (m *RegisterRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_RegisterRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_RegisterRequest proto.InternalMessageInfo
+
+func (m *RegisterRequest) GetUsername() string {
+	if m != nil {
+		return m.Username
+	}
+	return ""
+}
+
+func (m *RegisterRequest) GetPassword() string {
+	if m != nil {
+		return m.Password
+	}
+	return ""
+}
+
+func (m *RegisterRequest) GetFirstName() string {
+	if m != nil {
+		return m.FirstName
+	}
+	return ""
+}
+
+func (m *RegisterRequest) GetLastName() string {
+	if m != nil {
+		return m.LastName
+	}
+	return ""
+}
+
+func (m *RegisterRequest) GetEmail() string {
+	if m != nil {
+		return m.Email
+	}
+	return ""
+}
+
+type RegisterResponse struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *RegisterResponse) Reset()         { *m = RegisterResponse{} }
+func (m *RegisterResponse) String() string { return proto.CompactTextString(m) }
+func (*RegisterResponse) ProtoMessage()    {}
+func (*RegisterResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_4dc296cbfe5ffcd5, []int{5}
+}
+
+func (m *RegisterResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_RegisterResponse.Unmarshal(m, b)
+}
+func (m *RegisterResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_RegisterResponse.Marshal(b, m, deterministic)
+}
+func (m *RegisterResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_RegisterResponse.Merge(m, src)
+}
+func (m *RegisterResponse) XXX_Size() int {
+	return xxx_messageInfo_RegisterResponse.Size(m)
+}
+func (m *RegisterResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_RegisterResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_RegisterResponse proto.InternalMessageInfo
+
 func init() {
+	proto.RegisterType((*LoginRequest)(nil), "api.LoginRequest")
+	proto.RegisterType((*LoginResponse)(nil), "api.LoginResponse")
 	proto.RegisterType((*Message)(nil), "api.Message")
 	proto.RegisterType((*MessageResp)(nil), "api.MessageResp")
+	proto.RegisterType((*RegisterRequest)(nil), "api.RegisterRequest")
+	proto.RegisterType((*RegisterResponse)(nil), "api.RegisterResponse")
 }
 
 func init() { proto.RegisterFile("messages.proto", fileDescriptor_4dc296cbfe5ffcd5) }
 
 var fileDescriptor_4dc296cbfe5ffcd5 = []byte{
-	// 120 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xcb, 0x4d, 0x2d, 0x2e,
-	0x4e, 0x4c, 0x4f, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x4e, 0x2c, 0xc8, 0x54,
-	0x92, 0xe5, 0x62, 0xf7, 0x85, 0x08, 0x0b, 0x09, 0x71, 0xb1, 0xb8, 0x24, 0x96, 0x24, 0x4a, 0x30,
-	0x2a, 0x30, 0x6a, 0x70, 0x06, 0x81, 0xd9, 0x4a, 0x8a, 0x5c, 0xdc, 0x50, 0xe9, 0xa0, 0xd4, 0xe2,
-	0x02, 0x6c, 0x4a, 0x8c, 0xcc, 0xb9, 0x58, 0x9c, 0x33, 0x12, 0x4b, 0x84, 0xf4, 0xb9, 0xb8, 0x83,
-	0x53, 0xf3, 0x52, 0x60, 0xa6, 0xf1, 0xe8, 0x25, 0x16, 0x64, 0xea, 0x41, 0x79, 0x52, 0x02, 0xc8,
-	0x3c, 0x90, 0x51, 0x4a, 0x0c, 0x49, 0x6c, 0x60, 0x67, 0x18, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff,
-	0x5c, 0x91, 0xdc, 0x79, 0x98, 0x00, 0x00, 0x00,
+	// 293 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xa4, 0x92, 0xcf, 0x4a, 0xc3, 0x40,
+	0x10, 0xc6, 0x1b, 0x9b, 0x68, 0x3b, 0xad, 0x5a, 0x87, 0x0a, 0x21, 0x28, 0xe8, 0x82, 0xe0, 0x29,
+	0x4a, 0x3d, 0x88, 0x47, 0x51, 0x7b, 0xaa, 0x22, 0x51, 0x1f, 0x60, 0xa5, 0x4b, 0xba, 0x68, 0xb2,
+	0x31, 0xb3, 0xc5, 0x67, 0xf1, 0x6d, 0x65, 0xff, 0xc4, 0x86, 0xe2, 0xad, 0xb7, 0xfc, 0xe6, 0x9b,
+	0x6f, 0xf2, 0xcd, 0x24, 0xb0, 0x57, 0x08, 0x22, 0x9e, 0x0b, 0x4a, 0xab, 0x5a, 0x69, 0x85, 0x5d,
+	0x5e, 0x49, 0x36, 0x85, 0xe1, 0x4c, 0xe5, 0xb2, 0xcc, 0xc4, 0xd7, 0x52, 0x90, 0xc6, 0x04, 0x7a,
+	0x6f, 0x24, 0xea, 0x92, 0x17, 0x22, 0x0e, 0x4e, 0x82, 0xf3, 0x7e, 0xf6, 0xc7, 0x46, 0x7b, 0xe6,
+	0x44, 0xdf, 0xaa, 0x9e, 0xc7, 0x5b, 0x4e, 0x6b, 0x98, 0x9d, 0xc1, 0xae, 0x9f, 0x43, 0x95, 0x2a,
+	0x49, 0xe0, 0x18, 0xa2, 0x57, 0xf5, 0x21, 0x4a, 0x3f, 0xc5, 0x01, 0x3b, 0x86, 0x9d, 0x47, 0x97,
+	0x02, 0x11, 0xc2, 0x7b, 0xae, 0xb9, 0xd7, 0xed, 0x33, 0x3b, 0x85, 0x81, 0x97, 0xcd, 0x9c, 0x7f,
+	0x5b, 0x7e, 0x02, 0xd8, 0xcf, 0x44, 0x2e, 0x49, 0x8b, 0x7a, 0xc3, 0xd0, 0x78, 0x04, 0xfd, 0xa9,
+	0xac, 0x49, 0x3f, 0x19, 0x63, 0xd7, 0x8a, 0xab, 0x82, 0x71, 0xce, 0xb8, 0x17, 0x43, 0xe7, 0x6c,
+	0xd8, 0x6c, 0xf7, 0x50, 0x70, 0xf9, 0x19, 0x47, 0x6e, 0x3b, 0x0b, 0x0c, 0x61, 0xb4, 0x8a, 0xe6,
+	0xee, 0x30, 0xb9, 0x86, 0xf0, 0x6e, 0xc1, 0x35, 0x5e, 0xc0, 0xe0, 0x45, 0x94, 0xf3, 0x66, 0xfb,
+	0x61, 0xca, 0x2b, 0x99, 0x7a, 0x4a, 0x46, 0x6d, 0x32, 0x56, 0xd6, 0x99, 0x10, 0x84, 0xb7, 0x4b,
+	0xbd, 0xc0, 0x4b, 0x88, 0xec, 0x65, 0xf1, 0xc0, 0x36, 0xb5, 0xbf, 0x56, 0x82, 0xed, 0x92, 0x7b,
+	0x21, 0xeb, 0xe0, 0x0d, 0xf4, 0x9a, 0x18, 0x38, 0xb6, 0x1d, 0x6b, 0x07, 0x4b, 0x0e, 0xd7, 0xaa,
+	0x8d, 0xf5, 0x7d, 0xdb, 0xfe, 0x1a, 0x57, 0xbf, 0x01, 0x00, 0x00, 0xff, 0xff, 0x2d, 0x38, 0x0e,
+	0x9e, 0x2c, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -195,6 +400,114 @@ var _Chat_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SendMessage",
 			Handler:    _Chat_SendMessage_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "messages.proto",
+}
+
+// AuthClient is the client API for Auth service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type AuthClient interface {
+	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error)
+	Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
+}
+
+type authClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewAuthClient(cc *grpc.ClientConn) AuthClient {
+	return &authClient{cc}
+}
+
+func (c *authClient) Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error) {
+	out := new(LoginResponse)
+	err := c.cc.Invoke(ctx, "/api.Auth/Login", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *authClient) Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*RegisterResponse, error) {
+	out := new(RegisterResponse)
+	err := c.cc.Invoke(ctx, "/api.Auth/Register", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// AuthServer is the server API for Auth service.
+type AuthServer interface {
+	Login(context.Context, *LoginRequest) (*LoginResponse, error)
+	Register(context.Context, *RegisterRequest) (*RegisterResponse, error)
+}
+
+// UnimplementedAuthServer can be embedded to have forward compatible implementations.
+type UnimplementedAuthServer struct {
+}
+
+func (*UnimplementedAuthServer) Login(ctx context.Context, req *LoginRequest) (*LoginResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
+}
+func (*UnimplementedAuthServer) Register(ctx context.Context, req *RegisterRequest) (*RegisterResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Register not implemented")
+}
+
+func RegisterAuthServer(s *grpc.Server, srv AuthServer) {
+	s.RegisterService(&_Auth_serviceDesc, srv)
+}
+
+func _Auth_Login_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(LoginRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).Login(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/api.Auth/Login",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).Login(ctx, req.(*LoginRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Auth_Register_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RegisterRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).Register(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/api.Auth/Register",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).Register(ctx, req.(*RegisterRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Auth_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "api.Auth",
+	HandlerType: (*AuthServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Login",
+			Handler:    _Auth_Login_Handler,
+		},
+		{
+			MethodName: "Register",
+			Handler:    _Auth_Register_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
