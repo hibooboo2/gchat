267be023410d73dd2cd675329c36a4791cbf4f04
Chase <joli.chase@gmail.com>
2019-11-09 15:27:38 -0700

    Rgister and Login functionality added

--- client/main.go
+++ client/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
+	prompt "github.com/c-bata/go-prompt"
 	"github.com/hibooboo2/gchat/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
@@ -15,9 +17,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("can not connect with server %v", err)
 	}
-
 	// create stream
 	chatClient := api.NewChatClient(conn)
+	authClient := api.NewAuthClient(conn)
+	friendsClient := api.NewFriendsClient(conn)
+
+	es := &ExecutorScope{authClient: authClient}
+	p := prompt.New(es.executor, Commands)
+
+	p.Run()
 
 	ctx := context.Background()
 	// The following code is how you add metadata to the context for the server to use it.
@@ -30,17 +38,71 @@ func main() {
 
 	log.Println("response was:", resp)
 
-	a := api.NewAuthClient(conn)
-	regResp, err := a.Register(ctx, &api.RegisterRequest{})
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(regResp)
-	fc := api.NewFriendsClient(conn)
-	statusClient, err := fc.Status(ctx, nil)
+	statusClient, err := friendsClient.Status(ctx, nil)
 	if err != nil {
 		panic(err)
 	}
 	statusClient.Recv()
 
+}
+
+type ExecutorScope struct {
+	authClient api.AuthClient
+	ctx        context.Context
+}
+
+func (e *ExecutorScope) executor(t string) {
+	var err error
+	fmt.Println("You selected " + t)
+	switch t {
+	case "register":
+		err = reg(e.authClient)
+	case "login":
+		e.ctx, err = login(e.authClient)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func reg(authClient api.AuthClient) error {
+	ctx := context.Background()
+	req := &api.RegisterRequest{}
+	req.Email = prompt.Input("What is your email?", Empty)
+	req.Username = prompt.Input("What is your desired username?", Empty)
+	req.Password = prompt.Input("What do you want to set your password as?", Empty)
+	req.FirstName = prompt.Input("(Optional)What is your first name ?", Empty)
+	req.LastName = prompt.Input("(Optional)What is your last name?", Empty)
+	regResp, err := authClient.Register(ctx, req)
+	if err != nil {
+		return err
+	}
+	log.Println(regResp)
+	return nil
+}
+
+func Empty(d prompt.Document) []prompt.Suggest { return nil }
+
+func Commands(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "register", Description: "Register a user"},
+		{Text: "login", Description: "Login a user"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func login(authClient api.AuthClient) (context.Context, error) {
+
+	in := api.LoginRequest{
+		Username: prompt.Input("What is your username? ", Empty),
+		Password: prompt.Input("What is your password? ", Empty),
+	}
+	ctx := context.Background()
+	l, err := authClient.Login(ctx, &in)
+	if err != nil {
+		return nil, err
+	}
+	ctx = metadata.AppendToOutgoingContext(ctx, "TOKEN", l.Token)
+
+	return ctx, nil
 }
